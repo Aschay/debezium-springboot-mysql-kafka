a3c07796fd92db6b4c9cfddc2d3fb7205fba2d80
Aschay <ayadi.chayma@gmail.com>
2024-04-01 08:29:11 +0100

    configure debezium with redpanda

--- debeziumKafka/connector-script.sh
+++ debeziumKafka/connector-script.sh
@@ -19,3 +19,24 @@ curl -i -X POST localhost:8083/connectors -H 'Content-Type: application/json' -d
 curl -i -X GET localhost:8083/connectors
 
 curl -i -X DELETE localhost:8083/connectors/app-db-connector/
+
+
+# for redpanda
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json"  localhost:8083/connectors/ -d '{ 
+    "name": "app-db-connector",
+    "config": {
+      "connector.class": "io.debezium.connector.mysql.MySqlConnector",
+      "tasks.max": "1",
+      "database.hostname": "mysql",
+      "database.user": "root",
+      "database.password": "root",
+      "database.port": "3306",
+      "database.dbname": "customerdb",
+      "database.server.id": "20178",  
+      "topic.prefix": "dbserver" ,
+      "schema.history.internal.kafka.bootstrap.servers": "redpanda:9092",  
+      "schema.history.internal.kafka.topic": "schema-changes.customer"  
+    }
+}'
+docker exec -it redpanda rpk topic list
+docker exec -it redpanda rpk topic consume dbserver.customerdb.customer
